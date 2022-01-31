b50691c5f2082a5f154d145445f8a9c5d705f116
Legolas <geran.vanatta@gmail.com>
2022-01-30 23:40:01 -0600

    more local storage

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,28 @@
 // // // local storage variables // // // 
-searchBtn = document.querySelector("#search-btn");
-locationInput = document.querySelector("#location");
-recentSearches = document.querySelector("#city-searches");
-currentWeather = document.querySelector("#current-weather");
-currentForecast = document.querySelector("#current-forecast");
-searchBtn = document.querySelector("#search-btn");
+var searchBtn = document.querySelector("#search-btn");
+var locationInput = document.querySelector("#location");
+var recentSearches = document.querySelector("#city-searches");
+var currentWeather = document.querySelector("#current-weather");
+var currentForecast = document.querySelector("#current-forecast");
 
+var formSubmitHandler = function (event) {
+    event.preventDefault();
+
+    city = locationInput.value.trim();
+    searchHistory.push(city);
+    saveSearch();
+    loadSearch();
+}
+
+var saveSearch = function() {
+    localStorage.setItem("searchHistory", JSON.stringify(searchHistory));
+}
+
+var loadSearch = function() {
+    searchHistory = JSON.parse(getItem("searchHistory"));
+    if (!searchHistory) {
+        return false;
+    }
+};
 
 searchBtn.addEventListener("click", formSubmitHandler);
