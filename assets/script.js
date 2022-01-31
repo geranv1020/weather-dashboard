ccfe31a03793f9f50834c67c1e82b61e8eaac8ab
Legolas <geran.vanatta@gmail.com>
2022-01-30 23:25:55 -0600

    set up html, css and local storage

---
+++ assets/script.js
@@ -0,0 +1,10 @@
+// // // local storage variables // // // 
+searchBtn = document.querySelector("#search-btn");
+locationInput = document.querySelector("#location");
+recentSearches = document.querySelector("#city-searches");
+currentWeather = document.querySelector("#current-weather");
+currentForecast = document.querySelector("#current-forecast");
+searchBtn = document.querySelector("#search-btn");
+
+
+searchBtn.addEventListener("click", formSubmitHandler);
